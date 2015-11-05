cdddbb90c6a3cb1feb2353d4d970735bc5752a63
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-11-06 01:57:03 +0900

    Add many missing functions and add simple tests

--- src/hts_engine.jl
+++ src/hts_engine.jl
@@ -13,7 +13,8 @@ type HTS_Engine
         p = new(HTS_Condition(), HTS_Audio(), HTS_ModelSet(), HTS_Label(),
                 HTS_SStreamSet(), HTS_PStreamSet(), HTS_GStreamSet())
         initialize(p)
-        return p
+        finalizer(p, clear)
+        p
     end
 end
 
@@ -33,6 +34,8 @@ end
 
 ### Setter ###
 
+# TODO: remove dupulicate code
+
 for (name, argtype) in [
                          (:set_sampling_frequency, Csize_t),
                          (:set_fperiod, Csize_t),
@@ -40,8 +43,10 @@ for (name, argtype) in [
                          (:set_stop_flag, HTS_Boolean),
                          (:set_volume, Cdouble),
                          (:set_speed, Cdouble),
+                         (:set_phoneme_alignment_flag, HTS_Boolean),
                          (:set_alpha, Cdouble),
-                         (:set_beta, Cdouble)
+                         (:set_beta, Cdouble),
+                         (:add_half_tone, Cdouble)
                          ]
     fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
     @eval begin
@@ -52,33 +57,51 @@ for (name, argtype) in [
 end
 
 for (name, argt1, argt2) in [
-                         (:set_msd_threshold, Csize_t, Cdouble),
-                         (:set_gv_weight, Csize_t, Cdouble),
-                         ]
+                             (:set_msd_threshold, Csize_t, Cdouble),
+                             (:set_gv_weight, Csize_t, Cdouble),
+                             (:set_duration_interpolation_weight, Csize_t, Cdouble),
+                             ]
     fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
     @eval begin
-        function $name(engine::HTS_Engine, stream_index, val)
+        function $name(engine::HTS_Engine, index, val)
             @htscall($fsymbol, Void, (Ptr{HTS_Engine}, $argt1, $argt2),
-                     &engine, stream_index, val)
+                     &engine, index, val)
         end
     end
 end
 
+for (name, argt1, argt2, argt3) in [
+                                    (:set_parameter_interpolation_weight,
+                                     Csize_t, Csize_t, Cdouble),
+                                    (:set_gv_interpolation_weight,
+                                     Csize_t, Csize_t, Cdouble)
+                                    ]
+    fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
+    @eval begin
+        function $name(engine::HTS_Engine, voice_index, stream_index, val)
+            @htscall($fsymbol, Void, (Ptr{HTS_Engine}, $argt1, $argt2, $argt3),
+                     &engine, voiece_index, stream_index, val)
+        end
+    end
+end
+
+
 ### Getter ###
 
 for (name, rettype) in [
-                         (:get_sampling_frequency, Csize_t),
-                         (:get_fperiod, Csize_t),
-                         (:get_audio_buff_size, Csize_t),
-                         (:get_volume, Cdouble),
-                         (:get_alpha, Cdouble),
-                         (:get_beta, Cdouble),
-                         (:get_nvoices, Csize_t),
-                         (:get_nstream, Csize_t),
-                         (:get_nstate, Csize_t),
-                         (:get_total_frame, Csize_t),
-                         (:get_nsamples, Csize_t)
-                         ]
+                        (:get_sampling_frequency, Csize_t),
+                        (:get_fperiod, Csize_t),
+                        (:get_audio_buff_size, Csize_t),
+                        (:get_volume, Cdouble),
+                        (:get_alpha, Cdouble),
+                        (:get_beta, Cdouble),
+                        (:get_total_state, Csize_t),
+                        (:get_nvoices, Csize_t),
+                        (:get_nstream, Csize_t),
+                        (:get_nstate, Csize_t),
+                        (:get_total_frame, Csize_t),
+                        (:get_nsamples, Csize_t)
+                        ]
     fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
     @eval begin
         function $name(engine::HTS_Engine)
@@ -90,17 +113,34 @@ end
 
 for (name, rettype, argtype) in [
                                  (:get_msd_threshold, Cdouble, Csize_t),
-                                 (:get_gv_weight, Cdouble, Csize_t)
+                                 (:get_gv_weight, Cdouble, Csize_t),
+                                 (:get_duration_interpolation_weight,
+                                  Cdouble, Csize_t),
+                                 (:get_state_duration, Csize_t, Csize_t)
                                  ]
     fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
     @eval begin
-        function $name(engine::HTS_Engine, stream_index)
+        function $name(engine::HTS_Engine, index)
             @htscall($fsymbol, $rettype, (Ptr{HTS_Engine}, $argtype),
-                     &engine, stream_index)
+                     &engine, index)
         end
     end
 end
 
+for (name, rettype, argt1, argt2) in [
+                                      (:get_parameter_interpolation_weight,
+                                       Cdouble, Csize_t, Csize_t),
+                                      (:get_gv_interpolation_weight,
+                                       Cdouble, Csize_t, Csize_t)
+                                      ]
+    fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
+    @eval begin
+        function $name(engine::HTS_Engine, voice_index, stream_index)
+            @htscall($fsymbol, $rettype, (Ptr{HTS_Engine}, $argt1, $argt2),
+                     &engine, voiece_index, stream_index)
+        end
+    end
+end
 
 function load{T<:AbstractString}(engine::HTS_Engine, voices::Vector{T})
     for voice in voices
@@ -109,7 +149,7 @@ function load{T<:AbstractString}(engine::HTS_Engine, voices::Vector{T})
         end
     end
     c_voices = Vector{Vector{Cchar}}(length(voices))
-    for i in 1 : length(voices)
+    for i in 1:length(voices)
         c_voices[i] = collect(voices[i])
     end
     ret = @htscall(:HTS_Engine_load, HTS_Boolean,
@@ -133,9 +173,39 @@ function get_stop_flag(engine::HTS_Engine)
     convert(Bool, stop_flag)
 end
 
+function get_fullcontext_label_format(engine::HTS_Engine)
+    ret = @htscall(:HTS_Engine_get_fullcontext_label_format,
+                   Ptr{Cchar}, (Ptr{HTS_Engine},), &engine)
+    bytestring(ret)
+end
+
+function get_fullcontext_label_version(engine::HTS_Engine)
+    ret = @htscall(:HTS_Engine_get_fullcontext_label_version,
+                   Ptr{Cchar}, (Ptr{HTS_Engine},), &engine)
+    bytestring(ret)
+end
+
+function get_generated_parameter(engine::HTS_Engine,
+                                 stream_index, frame_index, vector_index)
+    @htscall(:HTS_Engine_get_generated_parameter, Cdouble,
+             (Ptr{HTS_Engine}, Csize_t, Csize_t, Csize_t),
+             &engine, stream_index, frame_index, vector_index)
+end
+
 function get_generated_speech(engine::HTS_Engine, index)
+    @assert index > 0 # 1-origin for Julia interface
     @htscall(:HTS_Engine_get_generated_speech, Cdouble,
-             (Ptr{HTS_Engine}, Csize_t), &engine, index)
+             (Ptr{HTS_Engine}, Csize_t), &engine, index-1)
+end
+
+function get_generated_speech(engine::HTS_Engine)
+    speech = Vector{Float64}(get_nsamples(engine))
+
+    @inbounds for i in eachindex(speech)
+        speech[i] = get_generated_speech(engine, i)
+    end
+
+    speech
 end
 
 ### Synthesize ###
@@ -152,10 +222,46 @@ function synthesize_from_fn(engine::HTS_Engine, labelpath)
     end
 end
 
-function save_riff(engine::HTS_Engine, wavpath)
-    fp = ccall(:fopen, Ptr{Void}, (Ptr{Cchar}, Ptr{Cchar}), wavpath, "wb")
+function synthesize_from_strings(engine::HTS_Engine, lines)
+    clines = Vector{Vector{Cchar}}(length(lines))
+    for i in 1:length(lines)
+        clines[i] = collect(lines[i])
+    end
+
+    ret = @htscall(:HTS_Engine_synthesize_from_strings, HTS_Boolean,
+                   (Ptr{HTS_Engine}, Ptr{Ptr{Cchar}}, Csize_t),
+                   &engine, clines, length(clines))
+    r = convert(Bool, ret)
+    if !r
+        error("failed to synthesize waveform")
+    end
+end
+
+### Save ###
+
+for (name, mode) = [
+                    (:save_information, "wt"),
+                    (:save_label, "wt"),
+                    (:save_generated_speech, "wb"),
+                    (:save_riff, "wb")
+                    ]
+    fsymbol = QuoteNode(symbol(:HTS_Engine_, name))
+    @eval begin
+        function $name(engine::HTS_Engine, path)
+            fp = ccall(:fopen, Ptr{Void}, (Ptr{Cchar}, Ptr{Cchar}),
+                       path, $mode)
+            @assert fp != C_NULL
+            @htscall($fsymbol, Void,
+                     (Ptr{HTS_Engine}, Ptr{Void}), &engine, fp)
+            ccall(:fclose, Void, (Ptr{Void},), fp)
+        end
+    end
+end
+
+function save_generated_parameter(engine::HTS_Engine, stream_index, path)
+    fp = ccall(:fopen, Ptr{Void}, (Ptr{Cchar}, Ptr{Cchar}), path, "wb")
     @assert fp != C_NULL
-    @htscall(:HTS_Engine_save_riff, Void,
+    @htscall(:HTS_Engine_save_generated_parameter, Void,
              (Ptr{HTS_Engine}, Ptr{Void}), &engine, fp)
     ccall(:fclose, Void, (Ptr{Void},), fp)
 end
--- test/hts_engine.jl
+++ test/hts_engine.jl
@@ -3,13 +3,61 @@ using Base.Test
 
 using WAV
 
-mei_htsvoice = joinpath(dirname(@__FILE__), "data", "mei_normal.htsvoice")
-labelpath = joinpath(dirname(@__FILE__), "data", "nitech_jp_atr503_m001_a01.lab")
+### Data ###
 
-function test_hts_engine_basics()
+mei_htsvoice = joinpath(dirname(@__FILE__), "data", "mei_normal.htsvoice")
+labelpath = joinpath(dirname(@__FILE__), "data","nitech_jp_atr503_m001_a01.lab")
+
+### Tests ###
+
+function test_hts_engine_synthesis_funcs()
+    engine = HTS_Engine(mei_htsvoice)
+
+    # From filepath
+    synthesize_from_fn(engine, labelpath)
+
+    synthesized_fn = get_generated_speech(engine)
+    refresh(engine)
+
+    # From file contents (lines)
+    open(labelpath) do f
+        synthesize_from_strings(engine, readlines(f))
+    end
+    synthesized_strings = get_generated_speech(engine)
+
+    # should be same
+    @test synthesized_fn == synthesized_strings
+end
+
+function test_hts_engine_fullcontext_label()
+    engine = HTS_Engine(mei_htsvoice)
+    @test get_fullcontext_label_format(engine) == "HTS_TTS_JPN"
+    @test get_fullcontext_label_version(engine) == "1.0"
+end
+
+function test_hts_engine_save()
+    engine = HTS_Engine(mei_htsvoice)
+    synthesize_from_fn(engine, labelpath)
+
+    for savefunc in [
+                 save_information,
+                 save_label,
+                 save_generated_speech,
+                 save_riff]
+        file = tempname()
+        savefunc(engine, file)
+        @test isfile(file)
+        rm(file)
+    end
+
+    clear(engine)
+end
+
+function test_hts_engine_synthesis_and_save_wav()
     # Load htsvoice
     engine = HTS_Engine(mei_htsvoice)
     @test get_sampling_frequency(engine) == 48000
+    @test get_fperiod(engine) == 240
 
     # Synthesize waveform from label file
     synthesize_from_fn(engine, labelpath)
@@ -23,17 +71,27 @@ function test_hts_engine_basics()
     x, fs = wavread(file, format="native")
     @test fs == get_sampling_frequency(engine)
     rm(file)
-
     @test length(x) == get_nsamples(engine)
 
+    # get generated waveform
+    synthesized = get_generated_speech(engine)
+    synthesized = map(Int, map(trunc, synthesized))
+    synthesized = map(x -> clamp(x, typemin(Int16), typemax(Int16)), synthesized)
+
     # check sample-by-sample correctess
-    for i in 1:get_nsamples(engine)
-        value_in_engine = trunc(Int, get_generated_speech(engine, i-1))
-        value_in_engine = clamp(value_in_engine, typemin(Int16), typemax(Int16))
-        value_in_wav = x[i]
-        @test value_in_wav == value_in_engine
-    end
+    @test all(x .== synthesized)
 end
 
-info("test hts_engine basics")
-test_hts_engine_basics()
+### Run tests ###
+
+info("test: hts_engine synthesis functions")
+test_hts_engine_synthesis_funcs()
+
+info("test: hts_engine fullcontext label")
+test_hts_engine_fullcontext_label()
+
+info("test: hts_engine save")
+test_hts_engine_save()
+
+info("test: hts_engine syntheis and save to wav")
+test_hts_engine_synthesis_and_save_wav()
